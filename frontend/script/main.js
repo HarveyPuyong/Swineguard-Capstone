a1dd80d3dd52032f180b36ec9fc64c0f1925208c
harvey puyong <harveyluslos80@gmail.com>
2025-04-24 16:52:38 +0800

    Make functional the sidenav when go to another section

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,3 +1,26 @@
+const sideNavLinks = document.querySelectorAll('.side-nav__link');
+const sections = document.querySelectorAll('section')
+
+sideNavLinks.forEach(nav => {
+  const navValue = nav.getAttribute('data-value');
+
+  nav.addEventListener('click', () => {
+    sections.forEach(section => {
+      const sectionId = section.id;
+      if(navValue === sectionId) section.style.display = 'block';
+      else section.style.display = 'none'  
+    });
+
+    sideNavLinks.forEach(nav => nav.classList.remove('active'));
+    nav.classList.add('active')
+  });
+})
+
+
+
+
+
+
 const inventoryNavLinks = document.querySelectorAll('.inventory-nav__link');
 const inventoryTables = document.querySelectorAll('.inventory-table');
 
@@ -7,10 +30,14 @@ inventoryNavLinks.forEach(nav => {
 
     inventoryTables.forEach(table => {
       const tableValue = table.getAttribute('data-value');
-      table.style.display = (navValue === tableValue) ? 'block' : 'none';
+      if(navValue === tableValue) table.style.display = 'block';
+      else table.style.display =  'none';
+     
     });
 
     inventoryNavLinks.forEach(link => link.classList.remove('active'));
     nav.classList.add('active');
   });
 });
+
+
