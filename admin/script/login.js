1bb1d3f901c728f7612e93d127e8e5c05da329d0
harvey puyong <harveyluslos80@gmail.com>
2025-06-01 20:43:05 +0800

    -Create separate folders for admin and user -Add login page for admin

---
+++ admin/script/login.js
@@ -0,0 +1,15 @@
+
+// Show and hide password 
+const passwordInput = document.getElementById('password-input');
+const togglePassword = document.querySelector('.toggle-password-eye');
+
+togglePassword.addEventListener('click', () => {
+  const eyeSlash = document.querySelector('.eye-slash');
+  eyeSlash.classList.toggle('active');
+  
+  if(eyeSlash.classList.contains('active')){
+    passwordInput.setAttribute('type', 'text');
+  }else{
+    passwordInput.setAttribute('type', 'password');
+  }
+});
