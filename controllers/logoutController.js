fd1b247683cc25bdc5e71ad3165bfca71ccd63a8
harvey puyong <harveyluslos80@gmail.com>
2025-06-16 21:56:56 +0800

    Add comments to loginAdmin and logout controller

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -2,22 +2,27 @@ const UserDB = require('./../models/userModel');
 const jwt = require('jsonwebtoken');
 
 const logoutController = async(req, res) => {
+  // get refresh token from cookies
   const cookies = req.cookies;
   if(!cookies?.jwt) return res.status(401).json({message: "Refresh Token not found"})
 
   const refreshToken = cookies.jwt;
 
   try{
+    // find user base sa refreshToken
     const foundUser = await UserDB.findOne({refreshToken: {$in: [refreshToken]} });
 
+    // kapag walang na find na user, aclear pa din yung jwt sa cookies
     if(!foundUser) {
       res.clearCookie('jwt', {httpOnly: true, sameSite: 'None', secure: process.env.NODE_ENV === 'production'});
       return res.status(403).json({message: "User not found or already logged out"});
     }
 
+    // tagtagin yung refreshToken sa foundUser
     foundUser.refreshToken.pull(refreshToken);
     await foundUser.save();
 
+    // a clear yung token sa cookies
     res.clearCookie('jwt', {httpOnly: true, sameSite: 'None', secure: process.env.NODE_ENV === 'production'});
     return res.status(200).json({message: 'Successfully Logout'});
   }catch(err) {
