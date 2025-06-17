df2e192ce6d4cc1a6240e4dca91dd60cb365375c
harvey puyong <harveyluslos80@gmail.com>
2025-06-17 14:57:54 +0800

    Impliment toggle sidenav

--- frontend/admin/script/appoinmentCoordinatorMain.js
+++ frontend/admin/script/appoinmentCoordinatorMain.js
@@ -1,9 +1,11 @@
+import setupHeader from "./header.js";
 import sideNavFuntionality from "./sidenav.js";
 import setupAppointmentSection from "./appointment.js";
 import setupMessagesSection from "./message.js";
 import setupTechniciansSection from "./technicians.js";
 import setupSettingsSection from "./setting.js";
 
+setupHeader();
 sideNavFuntionality();
 setupAppointmentSection();
 setupMessagesSection();
