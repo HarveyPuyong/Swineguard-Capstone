b745dc121525cacbc804dc778d56ba3aaa38048e
Robert Magno <magnorobert86@gmail.com>
2025-06-26 23:54:35 +0800

    Message features

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -4,8 +4,11 @@ const messageController = require('./../controllers/messageController');
 
 //const verifyJWT = require('../middlewares/verifyJWT'); Saka mo na ito paganahin sa front end mo sa header at authorization mo
 
-router.post('/send', messageController.sendMessage);
-router.get('/user/:id', messageController.getUserMessages);// Get user messages
-router.get('/all', messageController.getMessages);
+router.post('/send', verifyJWT, messageController.sendMessage);
+router.get('/user/:id', verifyJWT, messageController.getUserMessages);// Get user messages
+router.get('/all', verifyJWT, verifyRoles(ROLE_LIST.Admin,
+                                         ROLE_LIST.AppointmentCoordinator,
+                                         ROLE_LIST.InventoryCoordinator), 
+                                         messageController.getMessages);
 
 module.exports = router
