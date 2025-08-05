9e43f18b936b9cf51dc7b9501a6c3a17ecdc3f77
harvey puyong <harveyluslos80@gmail.com>
2025-08-05 21:46:59 +0800

    Done styling in swine mapping content

--- frontend/admin/script/swines/swine-mapping.js
+++ frontend/admin/script/swines/swine-mapping.js
@@ -9,12 +9,12 @@ const swineMapping = () => {
           label: 'Sample Data',
           data: [60, 75, 20, 22, 27, 5],
           backgroundColor: [
-            'rgba(100, 149, 237, 0.8)', // blue
-            'rgba(72, 255, 180, 0.8)',  // aqua green
-            'rgba(255, 105, 97, 0.8)',  // red
-            'rgba(144, 238, 144, 0.8)', // light green
-            'rgba(255, 255, 102, 0.8)', // yellow
-            'rgba(255, 180, 80, 0.8)'   // orange
+            'rgba(100, 149, 237, 0.8)', 
+            'rgba(72, 255, 180, 0.8)', 
+            'rgba(255, 105, 97, 0.8)',  
+            'rgba(144, 238, 144, 0.8)', 
+            'rgba(255, 255, 102, 0.8)', 
+            'rgba(255, 180, 80, 0.8)'   
           ],
           borderWidth: 1
         }]
